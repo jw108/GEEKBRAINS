9b308a012bd4c82b3ef12d25c7da9fe8a81dd60e
Chugunov <jw108mac@gmail.com>
2023-01-14 18:37:41 +0300

    Шаблон для решения задачи №4

---
+++ Seminars/TASKS/HomeWork/TASK#4/Program.cs
@@ -0,0 +1,23 @@
+﻿/* 
+Задача 4: Напишите программу, 
+которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+2, 3, 7 -> 7
+44 5 78 -> 78
+22 3 9 -> 22
+ */
+Console.Clear(); // Очистка тепрминала
+int a = 2, b = 5, c = 4;
+
+int max = 0;
+int mid = 0;
+int min = 0;
+
+// Операция ветвление
+
+// Вывод результата
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("mid = ");
+Console.WriteLine(mid);
+Console.Write("min = ");
+Console.WriteLine(min);
