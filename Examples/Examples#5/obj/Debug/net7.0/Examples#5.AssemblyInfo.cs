16f88ffa35964c6bfc55a4247ea47c804f0fe0f6
Chugunov <jw108mac@gmail.com>
2023-01-15 16:43:59 +0300

    Добавил Examples#2

--- Examples/Examples#5/obj/Debug/net7.0/Examples#5.AssemblyInfo.cs
+++ Examples/Examples#5/obj/Debug/net7.0/Examples#5.AssemblyInfo.cs
@@ -10,12 +10,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("TASK#1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Examples#5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("TASK#1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("TASK#1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Examples#5")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Examples#5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Создано классом WriteCodeFragment MSBuild.
