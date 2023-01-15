9030bd60d27f444e65b27e5145b3d12ba623c613
Chugunov <jw108mac@gmail.com>
2023-01-15 17:44:58 +0300

    Добавил решение домашнего задания. Задачи №4

--- Seminars/TASKS/HomeWork/TASK#4/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#4/Program.cs
@@ -1,23 +1,21 @@
 ﻿/* 
-Задача 4: Напишите программу, 
-которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
 2, 3, 7 -> 7
 44 5 78 -> 78
 22 3 9 -> 22
  */
 Console.Clear(); // Очистка тепрминала
-int a = 2, b = 5, c = 4;
+int x = 11, y = 8, c = 4;
 
 int max = 0;
-int mid = 0;
-int min = 0;
 
 // Операция ветвление
-
+if (x >= y && x >= c)
+    max = x;
+else if (y >= x && y >= c )
+    max = y;
+else if (c >= x && c >= y)
+    max = c;
 // Вывод результата
 Console.Write("max = ");
 Console.WriteLine(max);
-Console.Write("mid = ");
-Console.WriteLine(mid);
-Console.Write("min = ");
-Console.WriteLine(min);
