c9954c3b8a915588d386d44624d52797848f648d
Chugunov <jw108mac@gmail.com>
2023-01-17 22:50:25 +0300

    Добавил .gitignore

--- Seminars/TASKS/TASK#14/Program.cs
+++ Seminars/TASKS/TASK#14/Program.cs
@@ -12,11 +12,5 @@ Console.Clear();
 Console.WriteLine("Введите первое число: ");
 int a = Convert.ToInt32(Console.ReadLine());
 
-if ((a % 7 == 0) && (a % 23 == 0))
-{
-    Console.WriteLine("Число кратно обоим числам");
-}
-else 
-{
-    Console.WriteLine("Не кратно");
-}
+if ((a % 7 == 0) && (a % 23 == 0)) Console.WriteLine("Число кратно обоим числам");
+else Console.WriteLine("Не кратно");
