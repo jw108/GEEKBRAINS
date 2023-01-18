bf99a9f2fabb8fec845dd0419ff5e0324d5e5b7a
Chugunov <jw108mac@gmail.com>
2023-01-18 10:12:20 +0300

    Добавил решение ДЗ TASK#13

--- Seminars/TASKS/HomeWork/TASK#13/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#13/Program.cs
@@ -7,3 +7,17 @@
 
 32679 -> 6
  */
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num < 0)
+{
+    Console.WriteLine("Введите число больше нуля");
+}
+else if (num > 99)
+{
+    string num1 = Convert.ToString(num);
+    Console.WriteLine($"Третья цифра введенного числа --> {num1[2]}");
+}
+else Console.WriteLine("Третьей цифры нет");
