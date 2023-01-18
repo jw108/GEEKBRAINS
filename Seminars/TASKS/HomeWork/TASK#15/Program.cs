6f55e5dd80c7dbfc7b0b9e219d57be0ea2006067
Chugunov <jw108mac@gmail.com>
2023-01-18 11:28:49 +0300

    Добавил решение ДЗ TASK#15

--- Seminars/TASKS/HomeWork/TASK#15/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#15/Program.cs
@@ -5,3 +5,14 @@
 7 -> да
 1 -> нет
  */
+Console.Clear();
+Console.WriteLine("Введите число дня недели для проверки будний или выходной день: ");
+int num = Convert.ToInt32(Console.ReadLine());
+string[] array = { "Mondey", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+if (num < 1 || num > 7) Console.WriteLine("Введите корректное число дня недели от 1 до 7");
+else if (num == 6 || num == 7) Console.WriteLine($"Число {num} --> является выходным днем {array[num-1]}");
+else if (num >= 1) 
+{
+    num--; 
+    Console.WriteLine($"Введеное число это будний день {array[num]}");
+}
