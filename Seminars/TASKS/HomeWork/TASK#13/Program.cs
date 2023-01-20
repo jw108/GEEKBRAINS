d76979f33801ec6ca869ecfd6f5aff7a2061eed5
Chugunov <jw108mac@gmail.com>
2023-01-20 17:17:09 +0300

    Сделал работу над ошибками в ДЗ TASK#13

--- Seminars/TASKS/HomeWork/TASK#13/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#13/Program.cs
@@ -15,8 +15,16 @@ if (num < 0)
 {
     Console.WriteLine("Введите число больше нуля");
 }
-else if (num > 99)
+else if (num > 99 && num < 999)
 {
-    Console.WriteLine((num %10) %10 );
+    Console.WriteLine(num %10); // Сдвигает регистр на одно положение влево 
+}
+else if (num > 999 && num < 9999)
+{
+    Console.WriteLine((num % 100)/10); // Сдвигает регистр на два положения влево и удаляет все что стоит справа от него
+}
+else if (num > 9999 && num < 99999)
+{
+    Console.WriteLine((num%1000)/100); // Сдвигает регистр на три положения влево и удаляет все что стоит справа от него
 }
 else Console.WriteLine("Третьей цифры нет");
