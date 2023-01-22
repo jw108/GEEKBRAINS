9ac47ed9ce4bc9eb06afca4191bea8db54f03592
Chugunov <jw108mac@gmail.com>
2023-01-22 08:41:22 +0300

    Добавил решение в ДЗ TASK#19

--- Seminars/TASKS/HomeWork/TASK#19/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#19/Program.cs
@@ -11,4 +11,32 @@
 
 Через строку и int
  */
- 
+
+Console.Clear();
+int num1 = 23432;
+int num2 = 12821;
+Console.WriteLine("Введите пятизначное число");
+int userNum = int.Parse(Console.ReadLine());
+
+if (userNum < 999)
+{
+    Console.WriteLine("Введите корректное число");
+}
+
+if (userNum > 9999)
+{
+    if ((userNum / 10000) == (num1 % 10)) ;
+
+    else Console.WriteLine("Число не является палиндромным");
+
+    if (((userNum % 100) / 10) == (num1 % 10000) / 1000)
+    {
+        if (((userNum % 1000) / 100) == (num1 % 1000) / 100)
+        {
+            Console.WriteLine("Число палиндромное");
+        }
+    }
+
+}
+
+
