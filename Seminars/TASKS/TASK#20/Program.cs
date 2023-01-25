ae63c7fd65eac670cd495557be58f678f742a4bf
Chugunov <jw108mac@gmail.com>
2023-01-26 00:29:59 +0300

    Добавил решение TASK#20

--- Seminars/TASKS/TASK#20/Program.cs
+++ Seminars/TASKS/TASK#20/Program.cs
@@ -5,6 +5,11 @@
 A (3,6); B (2,1) -> 5,09 
 A (7,-5); B (1,-1) -> 7,21
  */
+double Distance(double x1, double y1, double x2, double y2)
+{
+ double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+ return Math.Round(length, 3);
+}
 Console.Clear();
 Console.Write("Ведите десятичное число X1: ");
 double x1 = Convert.ToDouble(Console.ReadLine());
@@ -15,36 +20,5 @@ double x2 = Convert.ToDouble(Console.ReadLine());
 Console.Write("Ведите десятичное число Y2: ");
 double y2 = Convert.ToDouble(Console.ReadLine());
 
-double distance = Math.Sqrt(Math.Sqrt(x2 - x1) + Math.Sqrt(y2 - y1));
-Console.WriteLine();
-
-/* 
-double Distance(double xA, double yA, double xB, double yB)
-{
- double length = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2));
- return Math.Round(length, 2);
-}
-Console.Write("Input an x-coordonate of point A: ");
-double xA = Convert.ToDouble(Console.ReadLine());
-Console.Write("Input an y-coordonate of point A: ");
-double yA = Convert.ToDouble(Console.ReadLine());
-Console.Write("Input an x-coordonate of point B: ");
-double xB = Convert.ToDouble(Console.ReadLine());
-Console.Write("Input an y-coordonate of point B: ");
-double yB = Convert.ToDouble(Console.ReadLine());
-double distanceBetweenAB = Distance(xA, yA, xB, yB);
-Console.WriteLine($"Distance between points A and B is {distanceBetweenAB}.");
-*/
-
-/*
-Console.WriteLine("Введите координаты точки 1: ");
-
-string[] sArr = Console.ReadLine().Split(' ');
-int num1 = Convert.ToInt32(sArr[0]);
-int num2 = Convert.ToInt32(sArr[1]);
-
-Console.WriteLine("Введите координаты точки 2: ");
-
-string[] sArr2 = Console.ReadLine().Split(' ');
-int num3 = Convert.ToInt32(sArr2[0]);
-int num4 = Convert.ToInt32(sArr2[1]);
+double dist = Distance(x1,y1,x2,y2);
+Console.WriteLine(dist);
