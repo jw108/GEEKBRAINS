3040d4d20bbbaa298ea9c94d2cb1f7a76ace9a3a
Chugunov <jw108mac@gmail.com>
2023-01-26 21:05:27 +0300

    Добавил решение ДЗ TASK#21

--- Seminars/TASKS/HomeWork/TASK#21/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#21/Program.cs
@@ -14,19 +14,22 @@ Console.Clear();
 //      double length = Math.Sqrt((X2 - X1)) + Math.Sqrt(Y2 - Y1) + Math.Sqrt(Z2 - Z1);
 //      return Math.Round(length, 2);
 // }
-
+double Distance(double x1, double y1, double z1, double x2, double y2, double z2)
+{
+    double length = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2) + Math.Pow(z2 - z1, 2));
+    return Math.Round(length, 3);
+}
 Console.WriteLine("Введите через пробел координаты для точки А в формате X Y Z ");
 string[] A = Console.ReadLine().Split(' ');
-double X1 = double.Parse(A[0]);
-double Y1 = Convert.ToDouble(A[1]);
-double Z1 = Convert.ToDouble(A[2]);
-Console.WriteLine($"X1= {X1}");
+double x1 = Convert.ToDouble(A[0]);
+double y1 = Convert.ToDouble(A[1]);
+double z1 = Convert.ToDouble(A[2]);
+
 Console.WriteLine("Введите через пробел координаты для точки B в формате X Y Z ");
 string[] B = Console.ReadLine().Split(' ');
-double X2 = Convert.ToDouble(B[0]);
-double Y2 = Convert.ToDouble(B[1]);
-double Z2 = Convert.ToDouble(B[2]);
-Console.WriteLine(Y2);
-double dist = Convert.ToDouble (Math.Sqrt((X2 - X1)) + Math.Sqrt(Y2 - Y1) + Math.Sqrt(Z2 - Z1));
-//double dist = Convert.ToDouble(Distance);
+double x2 = Convert.ToDouble(B[0]);
+double y2 = Convert.ToDouble(B[1]);
+double z2 = Convert.ToDouble(B[2]);
+
+double dist = Distance(x1, y1, z1, x2, y2, z2);
 Console.WriteLine(dist);
