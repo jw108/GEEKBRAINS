6da64881c1d28479b283802368beea9e82f9cc95
Chugunov <jw108mac@gmail.com>
2023-01-29 15:51:58 +0300

    Добавил решение ДЗ TASK#29

--- Seminars/TASKS/HomeWork/TASK#29/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#29/Program.cs
@@ -5,4 +5,31 @@
 * Напишите программу которая задает массив из N элементов в заданном пользовательском диапазоне и выводит их на экран
 */
 
+Console.Clear();
+Console.WriteLine("Введите кол-во элементов массива");
+int N = int.Parse(Console.ReadLine());
+int[] array = new int[N];
 
+FillArray(array);
+Console.Write(String.Join(", ", array));
+Console.Write(" -> ");
+PrintArray(array);
+Console.WriteLine();
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(1, 50);
+    }
+}
+
+void PrintArray(int[] array)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.Write(" ]");
+}
