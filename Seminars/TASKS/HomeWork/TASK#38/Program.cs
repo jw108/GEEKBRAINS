fbd2733d4853e25772a98cc05f35e851866edae7
Chugunov <jw108mac@gmail.com>
2023-01-30 16:41:14 +0300

    Заготовка ДЗ TASK#38

--- Seminars/TASKS/HomeWork/TASK#38/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#38/Program.cs
@@ -3,3 +3,24 @@
 
 [3 7 22 2 78] -> 76
 */
+Console.Clear();
+int[] array = new int[8];
+FillArray();
+PrintArray(array);
+
+void FillArray()
+{
+    for (int i = 0; i < array.Length; i++) array[i] = new Random().Next(0, 100);
+}
+
+void PrintArray(int[] arg)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arg.Length; i++)
+    {
+        Console.Write(arg[i] + " ");
+    }
+    Console.Write("\b ] --> ");
+    Console.WriteLine();
+}
+
