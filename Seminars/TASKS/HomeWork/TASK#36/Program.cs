baab242185c7dee752c414d01355fa4a2c2eef76
Chugunov <jw108mac@gmail.com>
2023-01-30 14:58:55 +0300

    Добавил решение ДЗ TASK#36

--- Seminars/TASKS/HomeWork/TASK#36/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#36/Program.cs
@@ -5,3 +5,38 @@
 
 [-4, -6, 89, 6] -> 0
 */
+Console.Clear();
+int[] array = new int[8];
+FillArray();
+PrintArray(array);
+int res = FindIndexes(array);
+Console.WriteLine(res);
+void FillArray()
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100, 100);
+    }
+}
+void PrintArray(int[] arg)
+{
+    Console.Write("[ ");
+    for (int i = 0; i < arg.Length; i++)
+    {
+        Console.Write(arg[i] + " ");
+    }
+    Console.Write("\b ] --> ");
+}
+Console.WriteLine();
+
+int  FindIndexes(int[] array)
+{   int result =0;
+Console.Write("[ ");
+    for (int i=1; i<array.Length;i+=2)
+    {
+        Console.Write(array[i] + " ");
+        result = result + array[i];
+    }
+    Console.Write("\b ] Сумма чисел стоящих на нечетных позициях массива --> ");
+    return result;
+}
