b2e46e0317d96dbba2d4723cafd14ffceb2ab674
Chugunov <jw108mac@gmail.com>
2023-02-02 16:41:40 +0300

    Добавил решение задачи TASK#41

--- Seminars/TASKS/HomeWork/TASK#41/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#41/Program.cs
@@ -5,3 +5,23 @@
 
 1, -7, 567, 89, 223-> 3
  */
+
+Console.Clear();
+Console.WriteLine("Введите числа через пробел для подсчета введенных положительных чисел: ");
+string[] nums = Console.ReadLine().Split(" ");
+int[] ints = Array.ConvertAll(nums, int.Parse); // Преобразование строкового массива в массив int
+//Convert(nums);
+int res = Count(ints);
+Console.WriteLine(res);
+
+
+int Count(int[] arg)
+{
+    int count = 0;
+
+    for (int i = 0; i < arg.Length; i++)
+    {
+        if (arg[i] > 0) count++;
+    }
+    return count;
+}
