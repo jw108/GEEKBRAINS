cd303773dabb3930fbd386b42ced03184b3afbb2
Chugunov <jw108mac@gmail.com>
2023-02-07 15:55:34 +0300

    Добавил решение ДЗ TASK#47

---
+++ Seminars/TASKS/HomeWork/TASK#47/Program.cs
@@ -0,0 +1,43 @@
+﻿/* 
+Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+m = 3, n = 4.
+
+0,5 7 -2 -0,2
+
+1 -3,3 8 -9,9
+
+8 7,8 -7,1 9
+*/
+Console.Clear();
+Console.Write("Задайте колличество строк: ");
+int row = Convert.ToInt32(Console.ReadLine());
+Console.Write("Задайте колличество столбцов: ");
+int col = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[row, col];
+CreateArray(row,col);
+PrintArray(array);
+Console.WriteLine();
+
+void CreateArray(int row, int col)
+{   Random random = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Convert.ToDouble(random.Next(-100,100))/10;
+        }
+    }
+}
+
+void PrintArray(double[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {   
+        Console.WriteLine("");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"[ {arr[i, j],4} ]");
+        }
+    }
+}
