1998b7db514efa11ba27e763942dc6408e75e56e
Chugunov <jw108mac@gmail.com>
2023-02-12 23:52:07 +0300

    Добавил решение ДЗ TASK#54

--- Seminars/TASKS/HomeWork/TASK#54/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#54/Program.cs
@@ -9,3 +9,63 @@
 9 5 3 2
 8 4 4 2
 */
+//Console.Clear();
+Console.WriteLine("Данная программа упорядочивает числа лежащие в строках по убыванию");
+
+int row = new Random().Next(3, 6);
+int col = new Random().Next(3, 6);
+Console.WriteLine();
+
+int[,] arr = new int[row, col];
+int temp = 0;
+FillArray(arr);
+PrintArray(arr);
+FromMaxInRows(arr);
+PrintArray(arr);
+void FillArray(int[,] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = random.Next(1, 20);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.WriteLine("");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],4}");
+        }
+    }
+    Console.WriteLine();
+}
+
+void FromMaxInRows(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  { 
+    for (int j = 0; j < array.GetLength(1); j++)
+    { 
+      for (int k = 0; k < array.GetLength(1) - 1; k++)
+      {
+        if (array[i, k] < array[i, k + 1])
+        { 
+          int temp = array[i, k + 1];
+          
+          array[i, k + 1] = array[i, k];
+          
+          array[i, k] = temp;
+          
+        }
+      }
+    }
+  }Console.WriteLine();
+}
+    
