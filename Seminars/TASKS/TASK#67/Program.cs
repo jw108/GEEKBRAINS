56454838da5f3852be5cc2278b66d92e645da6a2
Chugunov <jw108mac@gmail.com>
2023-02-13 22:00:22 +0300

    Сохранил все

---
+++ Seminars/TASKS/TASK#67/Program.cs
@@ -0,0 +1,16 @@
+﻿/*  
+Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+453 -> 12
+45 -> 9
+*/
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int PrintNumbers(int n)
+{
+    if (n == 0) return 0;
+    return n % 10 + PrintNumbers(n / 10);
+}
+
+Console.WriteLine(PrintNumbers(num));
