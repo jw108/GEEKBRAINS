8041ff83361f055fbf25ed874fe8c32ddebc5c10
Chugunov <jw108mac@gmail.com>
2023-02-14 15:26:40 +0300

    Добавил решение ДЗ TASK#58

--- Seminars/TASKS/HomeWork/TASK#58/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#58/Program.cs
@@ -7,3 +7,65 @@
 18 20
 15 18
 */
+
+Console.Clear();
+Console.WriteLine($"Программа, которая находит произведение двух матриц.");
+int row = 2;
+int col = 2;
+int col2 = 2;
+
+
+int[,] firstMartrix = new int[row, col];
+CreateArray(firstMartrix);
+Console.WriteLine($"\nПервая матрица:");
+PrintArray(firstMartrix);
+
+int[,] secomdMartrix = new int[col, col2];
+CreateArray(secomdMartrix);
+Console.WriteLine($"\nВторая матрица:");
+PrintArray(secomdMartrix);
+
+int[,] resultMatrix = new int[row,col2];
+
+MultiplyMatrix(firstMartrix, secomdMartrix, resultMatrix);
+Console.WriteLine($"\nПроизведение первой и второй матриц:");
+PrintArray(resultMatrix);
+
+void CreateArray(int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      array[i, j] = new Random().Next(1,10);
+    }
+  }
+}
+
+void PrintArray (int[,] array)
+{
+  for (int i = 0; i < array.GetLength(0); i++)
+  {
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+      Console.Write($"{array[i,j],5}");
+    }
+    Console.WriteLine();
+  }
+}
+
+void MultiplyMatrix(int[,] firstMartrix, int[,] secomdMartrix, int[,] resultMatrix)
+{
+  for (int i = 0; i < resultMatrix.GetLength(0); i++)
+  {
+    for (int j = 0; j < resultMatrix.GetLength(1); j++)
+    {
+      int sum = 0;
+      for (int k = 0; k < firstMartrix.GetLength(1); k++)
+      {
+        sum += firstMartrix[i,k] * secomdMartrix[k,j];
+      }
+      resultMatrix[i,j] = sum;
+    }
+  }
+}
