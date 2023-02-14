2d7a881733befce8a9fd1454fbead31b88ce6e09
Chugunov <jw108mac@gmail.com>
2023-02-14 13:43:53 +0300

    Добавил решение ДЗ TASK#56

--- Seminars/TASKS/HomeWork/TASK#56/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#56/Program.cs
@@ -11,5 +11,62 @@
 
 5 2 6 7
 
-Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка 
 */
+
+Console.Clear();
+Console.WriteLine("Данная программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов");
+Console.WriteLine();
+int row = 4;
+int col = 5;
+
+int[,] arr = new int[row, col];
+int temp = int.MaxValue;
+int index= 0;
+FillArray(arr);
+PrintArray(arr);
+SummEachRows(arr);
+Console.WriteLine();
+
+void FillArray(int[,] arr)
+{
+    Random random = new Random();
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            arr[i, j] = random.Next(1, 20);
+        }
+    }
+}
+
+void PrintArray(int[,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        Console.WriteLine("");
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            Console.Write($"{arr[i, j],4}");
+        }
+    }
+    Console.WriteLine();
+}
+
+void SummEachRows(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int summ = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            summ += array[i, j];
+        }
+        if (summ < temp )
+        {
+            temp = summ;
+            index = i;
+        }
+    }
+    Console.WriteLine($"Строка с наименьшей суммой чисел № {index+1}, её сумма равна {temp} ");
+}
