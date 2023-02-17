e359aa461672f9faf13ed385b7470857b888c581
Chugunov <jw108mac@gmail.com>
2023-02-17 17:22:58 +0300

    Добавил решение задачи TASK#68

--- Seminars/TASKS/HomeWork/TASK#68/Program.cs
+++ Seminars/TASKS/HomeWork/TASK#68/Program.cs
@@ -3,3 +3,16 @@
 m = 2, n = 3 -> A(m,n) = 9
 m = 3, n = 2 -> A(m,n) = 29
 */
+
+Console.Write("Введите чило m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите чило n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write($"Результатом вычисления функции Аккермана чисел:\nm = {m} и n = {n} будет --> A(m,n) = {Akkerman(m,n)}");
+Console.WriteLine();
+int Akkerman(int n, int m)
+{
+    if (n == 0)return Convert.ToInt32(m + 1);
+    else if (n != 0 && m == 0) return Akkerman(n - 1, 1);
+    return (Akkerman(n - 1, Akkerman(n, m - 1)));
+}
